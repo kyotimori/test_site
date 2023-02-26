4a394df6f8a0dc39929dff067228bb3c9a8a9022
kyotimori <irkitrav@gmail.com>
2023-02-26 17:30:00 +0300

    models added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=100)
+
+    def tag_name(self):
+        return '#' + self.caption
+    
+    def __str__(self):
+        return self.tag_name()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    excerpt = models.TextField()
+    img = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, default='', blank=True, null=False, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
+    tag = models.ManyToManyField(Tag)
--- blog/views.py
+++ blog/views.py
@@ -1,66 +1,12 @@
-from datetime import date
-
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
 
 
-post_data = [
-    {
-        'slug': 'sun-and-a-dog',
-        'img': 'dog.jpg',
-        'author': 'Nika',
-        'date': date(2017, 7, 20),
-        'title': 'Catching sun',
-        'excerpt': 'After so many cloudy and rainy days, the sun has finally come out. '
-                'Got out with Joey away from home, look how happy he is!',
-        'content': '''
-            Arrived ignorant miles its marry use dear education removing bed. 
-            Shed chicken much charmed nor comfort does desire welcomed smallness new melancholy announcing discovered sitting. 
-            Tedious every each along simplicity luckily speaking pursuit minuter spoil entreaties described effect juvenile merit tore. 
-            May dispatched affection change before how lasting. Incommode carried after engrossed above additions instantly unaffected branch draw evil winding northward. 
-        '''
-    },
-    {
-        'slug': 'cozy-evening',
-        'img': 'cacao.jpg',
-        'author': 'Nika',
-        'date': date(2017, 8, 5),
-        'title': 'Cozy evening',
-        'excerpt': 'After so many cloudy and rainy days, the sun has finally come out. '
-                'Got out with Joey away from home, look how happy he is!',
-        'content': ''''
-            Thing greatly branched done summer interested window described remainder. 
-            Imprudence tended sympathize want arise there such do wishing proposal cheered occasional attended. 
-            Thing sufficient stronger income behaviour change regret uneasy distant walk five park. 
-            Elsewhere windows place shyness musical believe enjoyment six. Discovery partiality equal introduced address each venture tall increasing estimable. 
-        '''
-    },
-    {
-        'slug': 'village-forest-field',
-        'img': 'village.jpg',
-        'author': 'Nika',
-        'date': date(2017, 8, 18),
-        'title': 'Countryside weekends',
-        'excerpt': "I haven't been to the village for a long time. "
-                "Finally, I can take a walk in the woods and in the field, make bouquets and pick berries. "
-                "I adore raspberries!",
-        'content': '''
-            Wishes chamber admire took invitation last so part. 
-            Marked admiration full greatly collected enquire. 
-            Loud hearted shutters appear each overcame me shed society conveying followed. 
-            Wished especially commanded barton remainder likewise unpacked table mutual rooms doubt surrounded natural mistake ecstatic. 
-            Behaviour talked confined suffering admitted sake minutes than resolving figure likewise required. 
-        '''
-    }
-]
-
-
 def index(request): 
     intro = 'Hiiiiii this is my blog!'
-    sorted_posts = sorted(post_data, key=lambda post: post['date'])
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by('-date')[:3]
     return render(request, 'blog/start_page.html', {
         'intro': intro,
         'data': latest_posts
@@ -68,15 +14,15 @@ def index(request):
 
 
 def posts(request):
-    post_number = 3
+    sorted_posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/posts.html', {
-        'range': range(post_number),
-        'data': post_data
+        'data': sorted_posts
     })
 
 
 def post_id(request, post_id):
-    identified_post = next(post for post in post_data if post['slug'] == post_id)
+    identified_post = get_object_or_404(Post, slug=post_id)
     return render(request, 'blog/post_page.html', {
-        'post': identified_post
+        'post': identified_post,
+        'post_tags': identified_post.tag.all()
     })
